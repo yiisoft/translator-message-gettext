9722cc0c7f9066f7fc6b6191b895f2c49361359d
darkdef <darkdef@gmail.com>
2025-04-20 14:36:00 +0300

    update

--- src/MessageSource.php
+++ src/MessageSource.php
@@ -69,8 +69,6 @@ final class MessageSource implements MessageReaderInterface
 
     private function setLocale(string $locale): void
     {
-        putenv('LC_ALL=' . $locale);
-        putenv('LANGUAGE=' . $locale);
         if (!setlocale(LC_ALL, $locale)) {
             throw new RuntimeException(sprintf('Locale "%s" cannot be set.', $locale));
         }
